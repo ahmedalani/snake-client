d14c674107b57e42bfcc11122b8c4fc0ec4f9c84
Ahmed Alani <s3leha@gmail.com>
2020-06-20 12:19:19 -0700

    play.js listening to key strokes from node after connecting to server, exit node when c is pressed

--- client.js
+++ client.js
@@ -17,6 +17,24 @@ const connect = function () {
     console.log('connection successfull!');
     conn.write('Name: AHA');
   })
+  conn.on('connect', () => {
+    // conn.write('Move: up');
+    // setInterval(() => {
+    //   conn.write('Move: up');
+    // }, 500)
+  })
+  conn.on('end', (e) => {
+    console.log('connection end!', e);
+  })
+  // conn.on('connect', () => {
+  //   conn.write('Move: down');
+  // })
+  // conn.on('connect', () => {
+  //   conn.write('Move: left');
+  // })
+  // conn.on('connect', () => {
+  //   conn.write('Move: right');
+  // })
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
--- play.js
+++ play.js
@@ -1,5 +1,25 @@
-/* eslint-disable func-names, no-console */
+/* eslint-disable */
 const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = function (data) {
+  if (data === 'c') {
+    process.exit();
+  }
+}
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+
+  return stdin;
+}
+setupInput();
